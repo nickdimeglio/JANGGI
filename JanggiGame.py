ecd915116cb4ddf20f658cbef99e1d232ba4513d
Nick DiMeglio <contact@nickdimeglio.com>
2021-02-27 16:55:02 -0500

    commit

---
+++ JanggiGame.py
@@ -0,0 +1,11 @@
+# Author: Nick DiMeglio
+# Date: 11 March 2021
+# Description: A class for playing the abstract board game Janggi
+
+
+
+
+class JanggiGame:
+	def __init__():
+		self._game_state = 'UNFINISHED'
+
