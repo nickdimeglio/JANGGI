894db93cbd12c044ee261adb63bdee54cf5838a8
Nick DiMeglio <contact@nickdimeglio.com>
2021-02-27 17:31:47 -0500

    Board, Piece classes created

--- JanggiGame.py
+++ JanggiGame.py
@@ -3,9 +3,28 @@
 # Description: A class for playing the abstract board game Janggi
 
 
+class Piece()
 
+class Board:
+	"""A class to represent a Janggi board"""
+
+	def __init__():
+	"""Create a new board"""
+	self._grid = []
+	for i in range(9): # 9 rows
+		col = []
+		for i in range(10): # 10 columns
+			col.append([])	# Each point on the grid starts empty
+		self._grid.append(col)
+
+	
 
 class JanggiGame:
 	def __init__():
 		self._game_state = 'UNFINISHED'
+		self._board = Board()
+
+
+
+
 
